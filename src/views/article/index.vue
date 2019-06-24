05ef40452a9f2a129c795fc451c799f8ce16f12c
2371827331@qq.com <2371827331@qq.com>
2019-06-24 22:15:11 +0800

    文章列表-创建组件并配置路由

---
+++ src/views/article/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>article component</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ArticleList',
+  data() {
+      return {}
+  }
+}
+</script>
+
+<style lang='less' scoped>
+
+</style>
+
+
