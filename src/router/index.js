c8d33fa63aa2d08a55d71286aafaa639f3e43ede
2371827331@qq.com <2371827331@qq.com>
2019-06-24 21:26:46 +0800

    使用路由拦截器处理页面访问权限

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     // {
     //   /*
@@ -41,3 +41,33 @@ export default new Router({
     }
   ]
 })
+/*
+所有路由导航都要经过这里
+to去哪儿
+from来自哪里
+next允许通行
+*/
+router.beforeEach((to,from,next) => {
+  const userInfo = window.localStorage.getItem('user_info')
+  // 如果是非/login页面，判断其登录状态
+  // console.log(to)
+  if (to.name !== 'login') {
+    if (!userInfo) {
+      // 如果没有登录，让其跳转到登录页面
+      next({name: 'login'})
+    }else {
+      // 如果登录了，就可以通过
+      next()
+    }
+  }else {
+    // 如果登录了就访问登录页
+    // 如果没有登录才允许访问
+    if (userInfo) {
+      next(false)           
+    }else {
+      next()
+    }
+  }
+})
+
+export default router
